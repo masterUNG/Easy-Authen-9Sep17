be2ef099b9c63728319dd0e92074c03828660779
MasterUNG <phrombutr@gmail.com>
2017-09-09 15:53:26 +0700

    Success

--- Easy Authen 6Sep/ViewController.swift
+++ Easy Authen 6Sep/ViewController.swift
@@ -9,6 +9,66 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let loginDictionary = ["user1": "pass1", "user2": "pass2"]
+    var userString = ""
+    var passwordString = ""
+    let alertArray = ["User False", "Password False", "Welcome User"]
+    
+    
+    
+    
+    
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    
+    @IBOutlet weak var userTextFeild: UITextField!
+    
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    @IBAction func loginButton(_ sender: Any) {
+        
+        userString = userTextFeild.text!
+        passwordString = passwordTextField.text!
+        
+        print("user ==> \(userString)")
+        print("password ==> \(passwordString)")
+        
+        let resultString = loginDictionary[userString]
+        print("result ==> \(String(describing: resultString))")
+        
+        
+        if (resultString == nil) {
+        
+            print("Have Nil")
+            messageLabel.text = alertArray[0]
+            
+        } else {
+            
+            //User True
+            let truePassword = resultString!
+            
+            if (passwordString == truePassword) {
+                messageLabel.text = alertArray[2]
+            } else {
+                messageLabel.text = alertArray[1]
+            }
+            
+            
+            
+        
+        }
+        
+        
+        
+        
+    }
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
